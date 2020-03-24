e84518254ed9288a39852c8a49b3ee9e673400c6
Jakub Rumpel <jakub@rumpel.pl>
2020-03-25 00:59:33 +0100

    changed request mode to no-cors

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,6 +24,7 @@ export default {
       const options = {
         url: 'https://api.cloudflare.com/client/v4/zones/8248231716cb84721a509111e1c2c6b3/purge_cache',
         method: 'POST',
+        mode: 'no-cors',
         headers: {
           'X-Auth-Email' : 'jakub@rumpel.pl',
           'X-Auth-Key' : '9zQG3k2mhSv6FTazF5qMXWpMQNrO-xvJjkxvRC77'
