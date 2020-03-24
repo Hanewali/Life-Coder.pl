0b6f2bd77b0591ade19781a305d1f1459859d612
Jakub Rumpel <jakub@rumpel.pl>
2020-03-24 23:34:33 +0100

    Completed clearCache netlify function

--- NetlifyFunctions/clearCache.js
+++ NetlifyFunctions/clearCache.js
@@ -1,16 +1,33 @@
 const request = require('request');
 
-console.log("test");
 
 exports.handler = function(event, context, callback){
-	var e = JSON.parse(event.body);
-
-	console.log("test2");
+	//var e = JSON.parse(event.body);
 	const { CLOUDFLARE_API_KEY: apikey } = process.env;
 
-	console.log("Netlify Env: ", apikey);
+	const options = {
+		url: 'https://api.cloudflare.com/client/v4/zones/8248231716cb84721a509111e1c2c6b3/purge_cache',
+		method: 'POST',
+		headers: {
+			'X-Auth-Email' : 'jakub@rumpel.pl',
+			'X-Auth-Key' : apikey
+		},
+		body: {
+			'purge_everything': true
+		}
 
-	console.log("payload", payload);
+	}
 
-	callback(null, {statusCode: 200});
+	
+
+	request(options, 
+	function(error, response, body){
+		if(!error, && response.statusCode == 200){
+			console.log('Succesfuly cleared cloudFlare cache');
+			callback(null, {statusCode: 200});
+		} else {
+			console.log('There was an error during cloudFlare cache clearing');
+			callback(error, { statusCode: response.statusCode, body: body});
+		}
+	});
 }
