67161da3404d24c9c9e38f947948d07b4bc90e0c
Jakub Rumpel <jakub.rumpel@veloce.pl>
2020-04-28 23:11:37 +0200

    Added router to main.js file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import { routes } from './router.js'
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes,
+  mode: "history",
+  scrollBehavior(to, from, savedPosition) {
+    if(savedPosition){
+      return savedPosition;
+    }
+    if(to.hash){
+      return {
+        selector: to.hash
+      }
+    }
+  }
+});
+
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
