f56152a7e801002106b29b16b2030a3278e44efc
Jakub Rumpel <jakub.rumpel@veloce.pl>
2020-05-20 16:42:27 +0200

    commented out gitPresentation temporal module

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-    <v-app id="inspire">
-      <app-header></app-header>
-      <app-git-presentation></app-git-presentation>
+    <v-app>
+        <app-header></app-header>
+        <!-- <app-git-presentation></app-git-presentation> -->
     </v-app>
   </div>
 </template>
 
 <script>
 import Header from './components/shared/Header'
-import GitPresentation from './components/GitPresentation.md'
+// import GitPresentation from './components/GitPresentation.md'
 
 export default {
   name: 'App',
   components: {
-    appHeader: Header,
-    appGitPresentation: GitPresentation
+    appHeader: Header
+    // appGitPresentation: GitPresentation
   }
 }
 </script>
