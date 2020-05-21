352efcc87ec386fb6368f863ee185036a3a20b9a
Jakub Rumpel <jakub.rumpel@veloce.pl>
2020-05-21 14:08:51 +0200

    Cleaning unnecesary comments

--- src/main.js
+++ src/main.js
@@ -2,16 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import { routes } from './router.js'
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import vuetify from '@/plugins/vuetify'
-
-// Vue.use(BootstrapVue)
-// Vue.use(IconsPlugin)
-// Vue.use(Vuetify)
-
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-// import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
