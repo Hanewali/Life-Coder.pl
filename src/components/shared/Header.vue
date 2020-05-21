2122471e38d27399080d2ab688aece684bceeeaa
Jakub Rumpel <jakub.rumpel@veloce.pl>
2020-05-21 16:57:24 +0200

    Creating basic header component

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,57 +1,33 @@
 <template>
-  <v-app-bar
-    app>
+  <v-app-bar app flat>
     <v-container>
-    <v-row>
-      <v-col cols="2">
-        <v-img max-height="100%" contain :src="image_src" id="site-logo"></v-img>
-      </v-col>
-      <v-col cols=10>
-        <v-btn text>test</v-btn>
-      </v-col>
-    </v-row>
+      <v-row>
+        <v-col cols="2">
+          <v-toolbar-title id="header-title">Life-Coder.pl</v-toolbar-title>
+        </v-col>
+        <v-col cols="1">
+          <v-btn text>Home</v-btn>
+        </v-col>
+        <v-col cols=1>
+          <v-btn text>Kontakt</v-btn>
+        </v-col>
+      </v-row>
     </v-container>
   </v-app-bar>
-  <!-- <div id="header">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand ml-5 mr-5" href="#"><img src="../../assets/shared/header/logo.png" alt="Life-Coder.pl logo file"></a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="https://life-coder.pl/">
-              Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div> -->
 </template>
 
 <script>
 export default {
   name: "Header",
-  data(){
-    return {
-      image_src: require("../../assets/shared/header/logo.png")
-    }
+  data() {
+    return { };
   }
 };
 </script>
 
 <style scoped>
 
+.v-toolbar__title {
+  font-size: 1.5em;
+}
 </style>
